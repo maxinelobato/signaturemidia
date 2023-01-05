b2f88b4fcf2ecafc0bf18e4c49a9b9e797c3fac7
maxinelobato <maxinelobato89@hotmail.com>
2023-01-05 18:37:13 -0300

    changes_all

--- src/components/AboutClients.tsx
+++ src/components/AboutClients.tsx
@@ -27,7 +27,7 @@ import { Humberto } from './Humberto';
 export function AboutClients() {
   return (
     <Box bgGradient="linear(to-br, gray.900, gray.900, gray.700 50%)">
-      <Container maxW={'7xl'} mt={-28}>
+      <Container maxW={'7xl'} mt={{ base: '-28', md: '-28' }}>
         <Humberto />
         <Angelica />
       </Container>
