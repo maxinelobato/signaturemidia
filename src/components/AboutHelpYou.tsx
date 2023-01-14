496bd4f96bdc42cd2a8a86cc7edafdf63e72b07b
maxinelobato <maxinelobato89@hotmail.com>
2023-01-14 15:18:42 -0300

    changes

--- src/components/AboutHelpYou.tsx
+++ src/components/AboutHelpYou.tsx
@@ -72,89 +72,82 @@ export function AboutHelpYou() {
   return (
     <>
       <Box
-        zIndex={0}
-        maxW="100%"
-        maxH="auto"
         bgImage="./img/bgmkt.webp"
         bgSize="cover"
         bgPosition="center"
+        bgRepeat="no-repeat"
       >
         <Box bgGradient="linear(to-b, blackAlpha.900, rgba(193, 94, 3, 0.5) 150%)">
           <Container maxW={'7xl'}>
-            <Stack minH={'100vh'}>
-              <VStack spacing={4}>
+            <VStack spacing={4}>
+              <Box
+                backdropFilter="auto"
+                backdropBlur="1rem"
+                bgColor="whiteAlpha.50"
+                shadow="xl"
+                border="1px solid"
+                borderColor="whiteAlpha.200"
+                rounded="lg"
+                alignItems="center"
+                display="flex"
+                mt="2"
+                p={5}
+              >
+                <Lightbulb size={36} weight="fill" />
                 <Box
-                  backdropFilter="auto"
-                  backdropBlur="1rem"
-                  bgColor="whiteAlpha.50"
-                  shadow="xl"
-                  border="1px solid"
-                  borderColor="whiteAlpha.200"
-                  rounded="lg"
-                  alignItems="center"
-                  display="flex"
-                  mt="2"
-                  p={5}
+                  ml="2"
+                  color="whiteAlpha.800"
+                  fontSize="lg"
+                  textAlign="center"
+                  fontWeight="bold"
                 >
-                  <Lightbulb size={36} weight="fill" />
-                  <Box
-                    ml="2"
-                    color="whiteAlpha.800"
-                    fontSize="lg"
-                    textAlign="center"
-                    fontWeight="bold"
-                  >
-                    Nossa Solução
-                  </Box>
+                  Nossa Solução
                 </Box>
-              </VStack>
-              <Stack textAlign={'center'} align={'center'} py={{ base: 12, md: 20 }}>
-                <Heading
-                  fontSize={{ base: '2xl', md: '3xl', lg: '4xl' }}
-                  textAlign={{ base: 'center', md: 'center', lg: 'center' }}
-                >
-                  <Text color="whiteAlpha.800">Você está feliz com a</Text>{' '}
-                  <Text color="orange.300" as="span" textShadow="#000 1px 1px">
-                    <i>captação de clientes</i> no seu negócio?
-                  </Text>{' '}
-                </Heading>
-              </Stack>
-              <Center>
-                <Container maxW={'2xl'}>
-                  <Stack textAlign={'center'}>
-                    <SimpleGrid
-                      columns={{ base: 1, md: 1 }}
-                      spacing={{ base: 5, lg: 5 }}
-                    >
-                      <StatsCard
-                        title={
-                          'Caso a resposta seja NÃO, nós da Signature Mídia iremos te mostrar como os nossos clientes alcançam mais clientes todos os meses'
-                        }
-                        icon={<NumberCircleOne size={'5em'} weight="duotone" />}
-                      />
-                      <StatsCard
-                        title={
-                          'A nossa missão é fazer você ter mais reconhecimento, posicionamento e escala para ter clientes qualificados fechando contratos com você'
-                        }
-                        icon={<NumberCircleTwo size={'5em'} weight="duotone" />}
-                      />
-                      <StatsCard
-                        title={
-                          'Esqueça a compra de seguidores, isso NÃO PAGA suas contas'
-                        }
-                        icon={<NumberCircleThree size={'5em'} weight="duotone" />}
-                      />
-                      <StatsCard
-                        title={
-                          'Ter a oportunidade de transformar o seu negócio, para que ele seja sólido na internet, trazendo solidez e recorrência'
-                        }
-                        icon={<NumberCircleFour size={'5em'} weight="duotone" />}
-                      />
-                    </SimpleGrid>
-                  </Stack>
-                </Container>
-              </Center>
+              </Box>
+            </VStack>
+            <Stack textAlign={'center'} align={'center'} py={{ base: 12, md: 20 }}>
+              <Heading
+                fontSize={{ base: '2xl', md: '3xl', lg: '4xl' }}
+                textAlign={{ base: 'center', md: 'center', lg: 'center' }}
+              >
+                <Text color="whiteAlpha.800">Você está feliz com a</Text>{' '}
+                <Text color="orange.300" as="span" textShadow="#000 1px 1px">
+                  <i>captação de clientes</i> no seu negócio?
+                </Text>{' '}
+              </Heading>
             </Stack>
+            <Center>
+              <Container maxW={'2xl'}>
+                <Stack textAlign={'center'}>
+                  <SimpleGrid columns={{ base: 1, md: 1 }} spacing={{ base: 5, lg: 5 }}>
+                    <StatsCard
+                      title={
+                        'Caso a resposta seja NÃO, nós da Signature Mídia iremos te mostrar como os nossos clientes alcançam mais clientes todos os meses'
+                      }
+                      icon={<NumberCircleOne size={'5em'} weight="duotone" />}
+                    />
+                    <StatsCard
+                      title={
+                        'A nossa missão é fazer você ter mais reconhecimento, posicionamento e escala para ter clientes qualificados fechando contratos com você'
+                      }
+                      icon={<NumberCircleTwo size={'5em'} weight="duotone" />}
+                    />
+                    <StatsCard
+                      title={
+                        'Esqueça a compra de seguidores, isso NÃO PAGA suas contas'
+                      }
+                      icon={<NumberCircleThree size={'5em'} weight="duotone" />}
+                    />
+                    <StatsCard
+                      title={
+                        'Ter a oportunidade de transformar o seu negócio, para que ele seja sólido na internet, trazendo solidez e recorrência'
+                      }
+                      icon={<NumberCircleFour size={'5em'} weight="duotone" />}
+                    />
+                  </SimpleGrid>
+                </Stack>
+              </Container>
+            </Center>
           </Container>
         </Box>
       </Box>
